39296412c666af65baa71e3b7729be2a7ef4f025
Martin Sandström <martin@marteinn.se>
2015-08-27 21:02:25 +0200

    Added linting

--- src/test/test-validate.js
+++ src/test/test-validate.js
@@ -4,11 +4,11 @@ import {validate} from "../lib/index";
 
 describe("Validator check", () => {
     it("checks names agains validator", () => {
-        assert(! validate("root"))
-        assert(! validate(" you"))
-        assert(! validate("SSL"))
-        assert(! validate(" sitemap"))
-        assert(! validate("terms-of-use"))
-        assert(validate("wwwvar"))
+        assert(! validate("root"));
+        assert(! validate(" you"));
+        assert(! validate("SSL"));
+        assert(! validate(" sitemap"));
+        assert(! validate("terms-of-use"));
+        assert(validate("wwwvar"));
     });
 });
